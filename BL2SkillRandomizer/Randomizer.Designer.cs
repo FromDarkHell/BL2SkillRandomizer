3f5ef0019a3aae3524ba564dadc8903f9acb3875
FromDarkHell <FromDarkHell@DARKHELL>
2017-07-16 17:53:39 -0500

    Add Zer0 Support

---
+++ BL2SkillRandomizer/Randomizer.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BL2SkillRandomizer
+{
+    partial class Randomizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.zero = new System.Windows.Forms.TabPage();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ZeroButton = new System.Windows.Forms.Button();
+            this.kronk = new System.Windows.Forms.TabPage();
+            this.Zer0 = new System.Windows.Forms.Timer(this.components);
+            this.KriegButton = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.Krieg = new System.Windows.Forms.Timer(this.components);
+            this.DialogSaver = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.zero.SuspendLayout();
+            this.kronk.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.zero);
+            this.tabControl1.Controls.Add(this.kronk);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(742, 461);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // zero
+            // 
+            this.zero.Controls.Add(this.progressBar1);
+            this.zero.Controls.Add(this.ZeroButton);
+            this.zero.Location = new System.Drawing.Point(4, 22);
+            this.zero.Name = "zero";
+            this.zero.Padding = new System.Windows.Forms.Padding(3);
+            this.zero.Size = new System.Drawing.Size(734, 435);
+            this.zero.TabIndex = 0;
+            this.zero.Text = "Zer0";
+            this.zero.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(7, 381);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(718, 46);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // ZeroButton
+            // 
+            this.ZeroButton.Location = new System.Drawing.Point(7, 7);
+            this.ZeroButton.Name = "ZeroButton";
+            this.ZeroButton.Size = new System.Drawing.Size(718, 67);
+            this.ZeroButton.TabIndex = 0;
+            this.ZeroButton.Text = "Randomize Skills";
+            this.ZeroButton.UseVisualStyleBackColor = true;
+            this.ZeroButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // kronk
+            // 
+            this.kronk.Controls.Add(this.progressBar2);
+            this.kronk.Controls.Add(this.KriegButton);
+            this.kronk.Location = new System.Drawing.Point(4, 22);
+            this.kronk.Name = "kronk";
+            this.kronk.Padding = new System.Windows.Forms.Padding(3);
+            this.kronk.Size = new System.Drawing.Size(734, 434);
+            this.kronk.TabIndex = 1;
+            this.kronk.Text = "Krieg";
+            this.kronk.UseVisualStyleBackColor = true;
+            // 
+            // Zer0
+            // 
+            this.Zer0.Interval = 1;
+            this.Zer0.Tick += new System.EventHandler(this.Loader_Tick);
+            // 
+            // KriegButton
+            // 
+            this.KriegButton.Location = new System.Drawing.Point(10, 3);
+            this.KriegButton.Name = "KriegButton";
+            this.KriegButton.Size = new System.Drawing.Size(718, 67);
+            this.KriegButton.TabIndex = 1;
+            this.KriegButton.Text = "Randomize Skills";
+            this.KriegButton.UseVisualStyleBackColor = true;
+            this.KriegButton.Click += new System.EventHandler(this.KriegButton_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(13, 385);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(718, 46);
+            this.progressBar2.TabIndex = 2;
+            // 
+            // Krieg
+            // 
+            this.Krieg.Tick += new System.EventHandler(this.Krieg_Tick);
+            // 
+            // Randomizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 464);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Randomizer";
+            this.Text = "Borderlands 2 Skill Randomizer";
+            this.Load += new System.EventHandler(this.Randomizer_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.zero.ResumeLayout(false);
+            this.kronk.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage zero;
+        private System.Windows.Forms.TabPage kronk;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button ZeroButton;
+        private System.Windows.Forms.Timer Zer0;
+        private System.Windows.Forms.Button KriegButton;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Timer Krieg;
+        private System.Windows.Forms.SaveFileDialog DialogSaver;
+    }
+}
+
---
+++ BL2SkillRandomizer/Randomizer.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BL2SkillRandomizer
+{
+    public partial class Randomizer : Form
+    {
+        Random.Org.Random r = new Random.Org.Random();
+        public Randomizer()
+        {
+            InitializeComponent();
+            r.UseLocalMode = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region Setup
+            DialogSaver.RestoreDirectory = true;
+            DialogSaver.DefaultExt = "hotfix";
+            DialogSaver.Filter = "Hotfix files (*.hotfix)|*.hotfix";
+            DialogSaver.Title = "Save Randomized Skill Tree";
+            DialogSaver.CheckFileExists = false;
+            DialogSaver.CheckPathExists = true;
+            #endregion
+            if (DialogSaver.ShowDialog() == DialogResult.OK)
+            {
+                string[] SkillList = { "GD_Assassin_Skills.Sniping.HeadShot", "GD_Assassin_Skills.Sniping.Optics", "GD_Assassin_Skills.Sniping.Killer", "GD_Assassin_Skills.Sniping.Precision", "GD_Assassin_Skills.Sniping.OneShotOneKill", "GD_Assassin_Skills.Sniping.Bore", "GD_Assassin_Skills.Sniping.Velocity", "GD_Assassin_Skills.Sniping.KillConfirmed", "GD_Assassin_Skills.Sniping.AtOneWithTheGun", "GD_Assassin_Skills.Sniping.CriticalAscention", "GD_Assassin_Skills.Cunning.FastHands", "GD_Assassin_Skills.Cunning.CounterStrike", "GD_Assassin_Skills.Cunning.Fearless", "GD_Assassin_Skills.Cunning.Ambush", "GD_Assassin_Skills.Cunning.RisingShot", "GD_Assassin_Skills.Cunning.DeathMark", "GD_Assassin_Skills.Cunning.Unforseen", "GD_Assassin_Skills.Cunning.Innervate", "GD_Assassin_Skills.Cunning.TwoFang", "GD_Assassin_Skills.Cunning.DeathBlossom", "GD_Assassin_Skills.Bloodshed.KillingBlow", "GD_Assassin_Skills.Bloodshed.IronHand", "GD_Assassin_Skills.Bloodshed.Grim", "GD_Assassin_Skills.Bloodshed.BeLikeWater", "GD_Assassin_Skills.Bloodshed.Followthrough", "GD_Assassin_Skills.Bloodshed.Execute", "GD_Assassin_Skills.Bloodshed.Backstab", "GD_Assassin_Skills.Bloodshed.Resurgence", "GD_Assassin_Skills.Bloodshed.LikeTheWind", "GD_Assassin_Skills.Bloodshed.ManyMustFall" };
+                using (var sw = new StreamWriter(DialogSaver.FileName))
+                {
+                    sw.WriteLine("start OnDemand GD_Assassin_Streaming");
+                    string SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("## Sniping ##" + Environment.NewLine);
+                    #region Sniping
+                    #region Sniping Tier - 1
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[0].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[0].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Sniping Tier - 2
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[1].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[1].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Sniping Tier - 3
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[2].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[2].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[2].Skills[2] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Sniping Tier - 4
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[3].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Sniping Tier - 5
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[4].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Sniping Tier - 6
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping Tiers[5].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #endregion
+                    sw.WriteLine("## Cunning ##" + Environment.NewLine);
+                    #region Cunning
+                    #region Cunning Tier - 1
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[0].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[0].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Cunning Tier - 2
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[1].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[1].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Cunning Tier - 3
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[2].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[2].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[2].Skills[2] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Cunning Tier - 4
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[3].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Cunning Tier - 5
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[4].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region  Cunning Tier - 6
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning Tiers[5].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #endregion
+                    sw.WriteLine("## Bloodshed ##" + Environment.NewLine);
+                    #region Bloodshed
+                    #region Bloodshed Tier - 1
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[0].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[0].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Bloodshed Tier - 2
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[1].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[1].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Bloodshed Tier - 3
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[2].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[2].Skills[1] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[2].Skills[2] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Bloodshed Tier - 4
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[3].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region Bloodshed Tier - 5
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[4].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    SkillList = SkillList.Where(s => s != SkillRandom).ToArray();
+                    SkillRandom = SkillList[r.Next(0, SkillList.Length)];
+                    #endregion
+                    #region  Bloodshed Tier - 6
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed Tiers[5].Skills[0] SkillDefinition'" + SkillRandom + "'" + Environment.NewLine);
+                    #endregion
+                    #region Skill Tree Name Randomizer
+                    string[] SkillTreeBranchNames = {"Snider","class", "mods", "redux", "dandy", "even.", ".", "Dexiduous", "Vorac", "A", "Skill Tree Name", "shake", "Milkshake", "Monty", "Python", "Atlantic", "Slot", "Machine", "BBC", "Bloodshed", "Pie", "good", "comedy", "British", "Larry", "Cable", "the", "sellout", "money", "stupid", "REKT", "beatle", "member", "withdrew", "film", "life", "of", "average", "former", "Tier", "6", "Skill", "paid", "back", "then", "residuals", "perpetuity", "who", "bark", "ruff", "ban", "shell","vision" };
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Sniping BranchName " + SkillTreeBranchNames[r.Next(0,SkillTreeBranchNames.Length)]);
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Cunning BranchName " + SkillTreeBranchNames[r.Next(0, SkillTreeBranchNames.Length)]);
+                    sw.WriteLine("set GD_Assassin_Skills.SkillTree.Branch_Bloodshed BranchName " + SkillTreeBranchNames[r.Next(0, SkillTreeBranchNames.Length)]);
+                    #endregion
+                    #endregion
+                }
+                MessageBox.Show("Zer0's Skill Tree Randomized!");
+            }
+        }
+        private void Loader_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+        }
+        private void KriegButton_Click(object sender, EventArgs e)
+        {
+            this.Krieg.Start();
+        }
+        private void Krieg_Tick(object sender, EventArgs e)
+        {
+            this.progressBar2.Increment(1);
+        }
+        private void Randomizer_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
